9e3326225b5d3791dc74395a72957eb0edb84c69
Swinal Kamble <swinal@tavisca.com>
2018-09-05 13:42:23 +0530

    Shoping Cart Using Solid Principles

---
+++ ShoppingCard-SOLID/Card-SOLID/CardMain.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card_SOLID
+{
+    class CardMain
+    {
+        static void Main(string[] args)
+        {
+            int f = 0;
+            List<Items> item = new List<Items>();
+            ActiveItem activeItem = new ActiveItem();
+            Payment payment = new Payment();
+
+            do
+            {
+                
+                Console.WriteLine("ENTER USER ID");
+                int uid = Console.Read();
+
+                Console.WriteLine("1. ADD ITEMS");
+                Console.WriteLine("2. REMOVE ITEMS");
+                Console.WriteLine("3. DO PAYMENT");
+                Console.WriteLine("4. TOTAL ITEMS");
+                Console.WriteLine("5. CLEAR CARD");
+                Console.WriteLine("6. DISPLAY PAYMENT DETAILS");
+                Console.WriteLine("7.EXIT");
+                int opt = Console.Read();
+                if(opt==1)
+                {
+                    activeItem.addItems(uid);
+                }
+                else if(opt==2)
+                {
+                    activeItem.removeItems(uid);
+                }
+                else if(opt==3)
+                {
+                    payment.paymentMethod(uid);
+                }
+                else if(opt==4)
+                {
+                    activeItem.totalItems(uid);
+                }
+                else if(opt==5)
+                {
+                    activeItem.clear(uid);
+                }
+                else if(opt==6)
+                {
+                    payment.displayPayment(uid);
+                }
+                else if(opt==7)
+                {
+                    f = 1;
+                }
+                
+            } while (f == 0);
+           
+
+        }
+    }
+}
---
+++ ShoppingCard-SOLID/Card-SOLID/Payment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card_SOLID
+{
+    public class Payment :Ipayment
+    {
+       
+        public void paymentMethod(int id)
+        {
+            Console.WriteLine("1. CASH");
+            Console.WriteLine("2. ONLINE");
+            int method = Console.Read();
+            if(method==1)
+            {
+                displayPayment(id);
+            }
+            else
+            {
+                displayPayment(id);
+            }
+        }
+        public void displayPayment(int id)
+        {
+            Console.WriteLine("TOTAL PAYMENT =" );
+        }
+      
+
+    }
+}
